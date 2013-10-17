93d5a8deeef12fdb29c58cb5bacb125b8e805542
Paul Whipp <paul.whipp@gmail.com>
2013-10-17 14:57:48 +1000

    Added tutorial python files and sample knowledge base

---
+++ ask.py
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+def ask(question,
+        responses={'yes': True,
+                   'y': True,
+                   'ye': True,
+                   'n': False,
+                   'no': False},
+        retries=3):
+    """ask question and return match in responses"""
+
+    # Add a space at the end of question if absent
+    if question[-1] != ' ':
+        question += ' '
+
+    answer = raw_input(question).lower()
+
+    try:
+        return responses[answer]
+    except KeyError:
+        print('Your answer must be one of {0}'.format(responses.keys()))
+        if retries > 0:
+            return ask(question, responses, retries - 1)
+        else:
+            raise
