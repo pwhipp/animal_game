0f154e0e75e6ffa956ed56e9238b13c15dfdcc32
pwhipp <paul.whipp@gmail.com>
2013-12-19 22:33:37 +1000

    Tidied up for pep8

--- animal.py
+++ animal.py
@@ -6,33 +6,36 @@ import pickle
 
 from animal_game.ask import ask
 
+
 def play_games():
     tree_root = load_tree()
-    question='Would you like to play the animal game?'
+    question = 'Would you like to play the animal game?'
     while ask(question):
         play_game(tree_root)
         question = 'Would you like to play again?'
     print('ok then, bye!')
     save_tree(tree_root)
-        
+
+
 def play_game(tree):
-    if tree.has_key('animal'): # Make the guess
+    if 'animal' in tree:  # Make the guess
         print('You are thinking of a {0}.'.format(tree['animal']))
         if ask('Am I right?'):
             print("I've won!")
         else:
             if ask('Oh dear, can you help me do better?'):
                 learn(tree)
-    else: # Ask the question and continue
+    else:  # Ask the question and continue
         if ask(tree['question']):
             play_game(tree['yes'])
         else:
             play_game(tree['no'])
 
+
 def learn(tree):
     animal = raw_input('What animal were you thinking of? ')
     question = raw_input('What yes/no question will tell between a {0} and a {1}? '.format(
-            animal, tree['animal']))
+        animal, tree['animal']))
     tree['question'] = question
     if ask('If you are thinking of a {0}, and I ask you "{1}", what would you answer?'.format(
             animal, question)):
@@ -41,21 +44,24 @@ def learn(tree):
     else:
         tree['yes'] = dict(animal=tree['animal'])
         tree['no'] = dict(animal=animal)
-    del(tree['animal'])
+    del (tree['animal'])
 
 
 kb_file = 'animal_knowledge.pickle'
+
+
 def load_tree():
     try:
         with open(kb_file, 'rb') as f:
             tree = pickle.load(f)
         print('Loaded "{0}"'.format(kb_file))
         return tree
-    except:
+    except IOError:
         print('Could not load "{0}", using default animal knowledge'.format(kb_file))
-        return  {'question': 'Does it have a long neck?',
-                 'yes': {'animal': 'giraffe'},
-                 'no': {'animal': 'sheep'}}
+        return {'question': 'Does it have a long neck?',
+                'yes': {'animal': 'giraffe'},
+                'no': {'animal': 'sheep'}}
+
 
 def save_tree(tree):
     try:
@@ -65,6 +71,7 @@ def save_tree(tree):
     except:
         print('Could not save "{0}".'.format(kb_file))
         raise
-    
+
+
 if __name__ == "__main__":
     play_games()
